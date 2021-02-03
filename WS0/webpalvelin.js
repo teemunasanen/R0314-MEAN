551e422ca84857c2757b0dc7c7f8453134d4ed6f
Teemu Näsänen <55226758+CyperNastee@users.noreply.github.com>
2021-02-03 22:17:33 +0200

    routes

--- WS0/webpalvelin.js
+++ WS0/webpalvelin.js
@@ -1,12 +1,33 @@
-
-
 var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('<h1>Nodemon</h1>');
-    response.end('<h1>Hello World!</h1>');
-    
-})
-.listen(8081);
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    if (request.url === "/") {
+      response.write("<h1>Nodemon</h1>");
+    } else if (request.url === "/helloworld") {
+      response.write("<h1>Node</h1>");
+    } else if (request.url === "/table") {
+      response.write("<h1>Table</h1>");
+      response.write(`<table border>
+       <tr>
+          <th>Name</th>
+          <th>Team</th>
+          <th>Number</th>
+        </tr>
+        <tr>
+          <td>Jari Litmanen</td>
+          <td>Ajax</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>Teemu Selanne</td>
+          <td>Winnipeg Jets</td>
+          <td>8</td>
+        </tr>
+      </table>`);
+    }
+    response.end("<h1>Hello World!</h1>");
+  })
+  .listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");
